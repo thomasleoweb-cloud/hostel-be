1c6ef5c7b00a46e5a205f8838123673694b27246
thomasleoweb-cloud <thomasleoweb@gmail.com>
2026-04-20 11:50:29 +0530

    dns issue fixed

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(` MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(` MongoDB connection error: ${error.message}`);
+    console.error(`❌ MongoDB connection error: ${error}`);
     process.exit(1);
   }
 };
